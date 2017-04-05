c90cdb49804921294a8657cf69b27e2a86dfab73
Richard K. Winzenried <winzenriedrk@appstate.edu>
2017-04-05 15:51:25 -0400

    Delete createWorking.sql

--- createWorking.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE working(
-	PName varchar(25) NOT NULL,
-	subteamName varchar(25) NOT NULL,
-	assignDate DATE,
-	CONSTRAINT PRIMARY KEY (PName, subteamName),
-	CONSTRAINT FOREIGN KEY (PName) REFERENCES project(projectName),
-	CONSTRAINT FOREIGN KEY (subteamName) REFERENCES subteam(teamName)
-);
