1928f59be95b50736d9d0e2214b3426c3ef7e14d
Richard K. Winzenried <winzenriedrk@appstate.edu>
2017-04-09 13:41:35 -0400

    Update createProject.sql

--- scripts/createProject.sql
+++ scripts/createProject.sql
@@ -8,7 +8,7 @@ CREATE TABLE project(
 	leaderID char(9),
 	subteamName varchar(25) NOT NULL,
 	CONSTRAINT PRIMARY KEY (projectName),
-	projPriority int(10) NOT NULL CHECK (projPriority <= 10),
+	projPriority int(10) NOT NULL CHECK (projPriority < 11) CHECK (projPriority > 0),
 	CONSTRAINT FOREIGN KEY (subteamName) REFERENCES subteam(teamName),
 	CONSTRAINT FOREIGN KEY (leaderID) REFERENCES member(bannerID)
 );
