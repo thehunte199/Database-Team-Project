20ce294c114d2ca8d77cd1a8f63d65fb17ebcd4e
Brian Smith <hoshiningen@gmail.com>
2017-04-23 12:54:17 -0400

    Query for parts used by a project

--- src/derived/num_subteams.sql
+++ src/derived/num_subteams.sql
@@ -1,3 +1,12 @@
-SELECT COUNT(*) FROM subteams;
+/* Returns the number rows stored in the subteams table. */
+SELECT COUNT(*)
+    FROM subteams;
 
-SELECT * FROM part WHERE
+/* Returns all of the parts used by a project and groups multiples of the same part w/ their count*/
+SELECT part.partNumber, part.partName, part.partLocation, count(part.partNumber), uses.dateUsed
+    FROM part, uses
+    WHERE part.partNumber = uses.partNumber AND
+          uses.projectName = "<SPECIFIED NAME>"
+    ORDER BY uses.dateUsed
+    GROUP BY part.partNumber;
+    
