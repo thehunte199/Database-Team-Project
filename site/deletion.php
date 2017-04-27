5016352e06a7dbc1156254d6eda8114d8ae9ff0e
patrickbeekman <patrickbeekman@github.com>
2017-04-26 23:51:42 -0400

    Adding the delete stuff that you did

--- site/deletion.php
+++ site/deletion.php
@@ -14,6 +14,31 @@ if (!mysql_query($sql,$mydb))
  die('Error: ' . mysql_error());
  }
 
-echo "1 record deleted";
+$result = @mysql_query("SELECT * FROM ".$_POST["table"], $mydb);
+ 
+     if ($result)
+     {
+         echo "<tr>";
+ 
+         for($i = 0; $i < mysql_num_fields($result); $i++)
+         {
+             $entity = mysql_fetch_field($result, $i);
+             echo "<th>". $entity->name . "</th>";
+          }
+         echo "</tr>";
+         while($tuple = mysql_fetch_row($result))
+         {
+            echo "<tr>";
+             for($i = 0; $i < mysql_num_fields($result); $i++)
+                echo "<td>".$tuple[$i]."</td>";
+             echo "</tr>";
+         }
+    }
+    else
+     {
+         die("Invalid query... some other text should go here: " . mysql_error());
+     }
+     mysql_free_result($result);
+
 mysql_close($mydb);
 ?> 
