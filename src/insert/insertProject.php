d48f85086105615fb03a63874ad7b8ff07a2631d
winzenriedrk <winzenriedrk@appstate.edu>
2017-04-27 21:02:06 -0400

    This should fix it.

---
+++ src/insert/insertProject.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="response.css"> 
+    </head>
+<body>
+<table>
+<?php
+    include("accessTest.txt");
+    mysql_select_db("DBS17T-9", $mydb);
+    $sql="INSERT INTO project(projectName,completedDate,subteamName,startDate,projDesc,assignDate,dueDate,projPriority,leaderID)
+VALUES
+('$_POST[projectName]','$_POST[completedDate]','$_POST[subteamName]','$_POST[startDate]','$_POST[projDesc]','$_POST[assignDate]','$POST_[dueDate]','$POST_[projPriority]','$POST_[leaderID]')";
+    $result = @mysql_query($sql, $mydb);
+    
+    if ($result)
+    {
+        echo "<tr>";
+        
+        for($i = 0; $i < mysql_num_fields($result); $i++)
+        {
+            $entity = mysql_fetch_field($result, $i);
+            echo "<th>". $entity->name . "</th>";  
+        }
+        echo "</tr>";
+        while($tuple = mysql_fetch_row($result))
+        {
+            echo "<tr>";
+            
+            for($i = 0; $i < mysql_num_fields($result); $i++)
+                echo "<td>".$tuple[$i]."</td>";   
+            echo "</tr>";
+        }
+    }
+    else
+    {
+        die("Invalid query... some other text should go here: " . mysql_error());
+    }
+    mysql_free_result($result);
+?>
+</table>
+</body>
+</html>
