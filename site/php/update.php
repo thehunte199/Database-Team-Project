233e979ef253c8651ee195370546ab800d2debad
James Ward <wardja2@appstate.edu>
2017-04-30 00:00:15 -0400

    almost have the update script working

---
+++ site/php/update.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/response.css">
+    </head>
+<body>
+<table>
+<?php
+    include("access.txt");
+    mysql_select_db("wardja2", $mydb);
+    $result = @mysql_query("UPDATE " .POST["tableDropdown"] . " SET " .$_POST["attributeDropdownSet"] . "=" .$_POST["updateValue"] . " WHERE " .$_POST["attributeDropdownWhere"] . " ".$_POST["compOperator"] ." " .$_POST["condition"], $mydb);
+
+    if ($result)
+    {
+        echo "<tr>";
+
+        for($i = 0; $i < mysql_num_fields($result); $i++)
+        {
+            $entity = mysql_fetch_field($result, $i);
+            echo "<th>". $entity->name . "</th>";
+        }
+        echo "</tr>";
+        while($tuple = mysql_fetch_row($result))
+        {
+            echo "<tr>";
+
+            for($i = 0; $i < mysql_num_fields($result); $i++)
+                echo "<td>".$tuple[$i]."</td>";
+            echo "</tr>";
+        }
+    }
+    else
+    {
+        die("Invalid query... some other text should go here: " . mysql_error());
+    }
+    mysql_free_result($result);
+?>
+</table>
+</body>
+</html>
